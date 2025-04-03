d782c7d824f08a8d71aa2f6f703f20c0d29bea9d
joaosantoz <jvsantosdev@gmail.com>
2025-04-03 07:25:16 -0300

    chore: remove unused eslint-config-prettier lib

--- eslint.config.js
+++ eslint.config.js
@@ -2,18 +2,11 @@
 const eslint = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
-const eslintPlugginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = tseslint.config(
   {
     files: ['**/*.ts'],
-    extends: [
-      eslint.configs.recommended,
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.stylistic,
-      ...angular.configs.tsRecommended,
-      eslintPlugginPrettierRecommended
-    ],
+    extends: [eslint.configs.recommended, ...tseslint.configs.recommended, ...tseslint.configs.stylistic, ...angular.configs.tsRecommended],
     processor: angular.processInlineTemplates,
     rules: {
       '@angular-eslint/directive-selector': [
