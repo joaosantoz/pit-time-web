1e3fcf0595064c98bfa81d291ccb8adea0df001e
joaosantoz <jvsantosdev@gmail.com>
2025-04-03 12:12:04 -0300

    chore: change karma reporter

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,3 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -9,24 +6,55 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-mocha-reporter'),
+      require('karma-spec-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+
+    files: ['src/**/*.ts', 'src/**/*.spec.ts'],
+
+    preprocessors: {
+      'src/app/**/!(*.spec|*.mock|*.module).ts': ['coverage']
+    },
+
     client: {
       jasmine: {
-        random: false
-      }
+        random: false,
+        failFast: true
+      },
+      clearContext: false
     },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
-    },
+
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/pit-time-web'),
       subdir: '.',
-      reporters: [{ type: 'html' }, { type: 'text-summary' }]
+      reporters: [{ type: 'html' }, { type: 'text-summary' }, { type: 'lcovonly' }],
+      includeAllSources: true,
+      instrumenterOptions: {
+        istanbul: {
+          preserveComments: true,
+          noCompact: true,
+          produceSourceMap: true,
+          ignoreClassMethods: []
+        }
+      },
+      check: {
+        global: {
+          statements: 80,
+          branches: 80,
+          functions: 80,
+          lines: 80
+        }
+      },
+      watermarks: {
+        statements: [50, 75],
+        functions: [50, 75],
+        branches: [50, 75],
+        lines: [50, 75]
+      }
     },
-    reporters: ['mocha', 'progress', 'kjhtml'],
+
+    reporters: ['spec', 'progress', 'kjhtml', 'coverage'],
     mochaReporter: {
       output: 'autowatch'
     },
