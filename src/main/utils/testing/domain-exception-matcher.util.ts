95c753ab69fcdd74d6188e5785844a24586aeb91
joaosantoz <jvsantosdev@gmail.com>
2025-04-04 14:19:39 -0300

    refactor: remove unused testing util

--- src/main/utils/testing/domain-exception-matcher.util.ts
+++
@@ -1,6 +0,0 @@
-import { ExceptionCode } from '@domain/enums/exception-code.enum';
-import { DomainException } from '@domain/exceptions/domain.exception';
-
-export const createDomainExceptionMatcher = (expectedMessage: string, expectedCode: ExceptionCode) => {
-  return (error: unknown) => error instanceof DomainException && error.message === expectedMessage && error.code === expectedCode;
-};
